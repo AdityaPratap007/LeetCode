a81a0e512227e84fc804e3aec90266e90a8a6adf
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-05-30 09:44:01 +0530

    Time: 29 ms (94.51%), Space: 74.3 MB (39.6%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -3,20 +3,22 @@ public:
     int longestPalindromeSubseq(string s) {
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
+        
         int n = s.length();
+
         vector<vector<int>> dp(n, vector<int>(n, 0));
-        for (int i = n - 1; i >= 0; --i) {
-            for (int j = i; j < n; ++j) {
-                if (i == j) {
-                    dp[i][j] = 1;
-                } else if (s[i] == s[j]) {
-                    dp[i][j] = 2 + (i + 1 <= j - 1 ? dp[i + 1][j - 1] : 0);
+
+        for (int i = n-1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i+1; j < n; j++) {
+                if (s[i] == s[j]) {
+                    dp[i][j] = dp[i+1][j-1] + 2;
                 } else {
-                    dp[i][j] = std::max((i + 1 <= j ? dp[i + 1][j] : 0),
-                                   (i <= j - 1 ? dp[i][j - 1] : 0));
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
                 }
             }
         }
-        return dp[0][n - 1];
+
+        return dp[0][n-1];
     }
 };
