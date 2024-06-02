f0b942e1e290612760110ed9518da22de9c1e3b7
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-06-02 21:05:04 +0530

    Time: 4 ms (27.91%), Space: 7.9 MB (7.39%) - LeetHub

---
+++ 3379-score-of-a-string/3379-score-of-a-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+        int sum=0;
+        for(auto i=0;i<s.size()-1;i++){
+            sum+=abs((s[i]-'0')-(s[i+1]-'0'));
+        }
+        return sum;
+    }
+};
