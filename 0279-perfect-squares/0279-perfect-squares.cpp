e9867964920769e808b245b702da9c2664196200
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-06-05 10:18:18 +0530

    Time: 38 ms (84.44%), Space: 10.5 MB (67.38%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numSquares(int n) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
         vector<int> dp(n + 1, INT_MAX);
         dp[0] = 0;
         
