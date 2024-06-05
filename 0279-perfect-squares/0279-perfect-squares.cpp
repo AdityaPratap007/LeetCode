63c346cd2e5cb5679bc292a814cdb1eeb9f3d0c9
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-06-05 10:27:37 +0530

    Time: 33 ms (89.43%), Space: 10.5 MB (81.34%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -8,7 +8,7 @@ public:
         
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j * j <= i; j++) {
-                dp[i] = min(dp[i], dp[i - j * j] + 1);
+                dp[i] = std::min(dp[i], dp[i - j * j] + 1);
             }
         }
         return dp[n];
