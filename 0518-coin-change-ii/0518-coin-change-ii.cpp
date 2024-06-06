05d8bc894a3aa19c4fb6ba5cf2f3786b8075e3bc
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-06-06 09:28:39 +0530

    Time: 16 ms (30.46%), Space: 19.9 MB (42.31%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -3,13 +3,20 @@ public:
     int change(int amount, vector<int>& coins) {
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
-        vector<int> dp(amount+1,0);
-        dp[0]=1;
-        for(int coin : coins){
-            for(int j=coin;j<=amount;j++){
-                dp[j]+=dp[j-coin];
+        
+        vector<vector<int>> dp(coins.size()+1,vector<int>(amount+1,0));
+        dp[0][0] = 1;
+    for (int i = 1; i <= coins.size(); i++) {
+        for (int j = 0; j <= amount; j++) {
+ 
+            dp[i][j] += dp[i - 1][j];
+ 
+            if ((j - coins[i - 1]) >= 0) {
+ 
+                dp[i][j] += dp[i][j - coins[i - 1]];
             }
         }
-        return dp[amount];
+    }
+    return dp[coins.size()][amount];
     }
 };
