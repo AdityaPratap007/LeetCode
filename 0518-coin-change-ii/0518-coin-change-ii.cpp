68de4b00f2d2b9417610749433868dc32a563c5e
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-06-06 09:19:42 +0530

    Time: 5 ms (79.56%), Space: 8.5 MB (84.25%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
         vector<int> dp(amount+1,0);
         dp[0]=1;
         for(int coin : coins){
