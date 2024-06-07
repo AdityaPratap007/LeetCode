06c2bb186c4741610d88a52cb7fb4f2f01249c2d
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-06-07 10:28:45 +0530

    Time: 36 ms (31.14%), Space: 29.7 MB (49.21%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     
     int minInsertions(string s) {
-        std::ios_base::sync_with_stdio(false);
-        std::cin.tie(nullptr);
+       
         int n = s.length();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1,0));
+        std::vector<vector<int>> dp(n + 1, vector<int>(n + 1,0));
         for (int i=1;i<=n;i++){
             for (int j=1;j<=n;j++){
-                dp[i][j]=s[i-1]==s[n-j]?1+dp[i-1][j-1]:max(dp[i-1][j],dp[i][j-1]);
+                dp[i][j]=s[i-1]==s[n-j]?1+dp[i-1][j-1]:std::max(dp[i-1][j],dp[i][j-1]);
             }
         }
         return n-dp[n][n];
