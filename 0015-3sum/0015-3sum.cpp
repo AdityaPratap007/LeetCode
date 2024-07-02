5c52baef0980f1cc6d3d3d9ae22ec7d28acf7600
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-07-02 11:26:47 +0530

    Time: 41 ms (99.55%), Space: 27.1 MB (79.61%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,24 +2,32 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& num) {
         vector<vector<int>> res;
-        std:: sort(num.begin(),num.end());
-        for(int i=0;i<num.size();i++){
-            int target= -num[i];
-            int front =i+1;
-            int back=num.size()-1;
-            while(front<back){
-                int sum=num[front]+num[back];
-                if(sum<target) front++;
-                else if(sum>target) back--;
-                else {
-                    vector<int> triplet ={num[i],num[front],num[back]};
-                    res.push_back(triplet);
+        sort(num.begin(), num.end());
 
-                    while(front<back && num[front]==triplet[1]) front++;
-                    while(front<back && num[back]==triplet[2]) back--;
+        for (int i = 0; i < num.size(); i++) {
+            if (i > 0 && num[i] == num[i-1]) continue;
+
+            int target = -num[i];
+            int front = i + 1;
+            int back = num.size() - 1;
+
+            while (front < back) {
+                int sum = num[front] + num[back];
+
+                if (sum < target) {
+                    front++;
+                } else if (sum > target) {
+                    back--;
+                } else {
+                    res.push_back({num[i], num[front], num[back]});
+
+                    while (front < back && num[front] == num[front + 1]) front++;
+                    while (front < back && num[back] == num[back - 1]) back--;
+                    
+                    front++;
+                    back--;
                 }
             }
-            while(i+1<num.size() && num[i+1]==num[i]) i++;
         }
         return res;
     }
