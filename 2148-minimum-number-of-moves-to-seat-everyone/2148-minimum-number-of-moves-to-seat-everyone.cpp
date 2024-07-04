bc88f9cd7d07800bff9f20108f8263cd82a71005
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-07-04 09:55:39 +0530

    Time: 8 ms (38%), Space: 21 MB (19.51%) - LeetHub

--- 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/2148-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
         sort(students.begin(),students.end());
         sort(seats.begin(),seats.end());
         int x=0;
