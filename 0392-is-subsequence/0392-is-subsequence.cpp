6c1931062beb9d4563de2fe9895cea61b63f3512
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-07-04 10:20:17 +0530

    Time: 3 ms (47.39%), Space: 7.5 MB (32.35%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        // Edge case: if s is empty, it is always a subsequence of t
+        if (s.empty()) return true;
+        
+        // Edge case: if t is empty and s is not empty, s cannot be a subsequence of t
+        if (t.empty()) return false;
+        
+        int i = 0, j = 0;
+        while (j < t.size()) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+            if (i >= s.size()) return true;
+            j++;
+        }
+        return i >= s.size();
+    }
+};
