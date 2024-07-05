fa17c957a72b904604a9260c37e4226127a93cbb
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-07-05 10:17:25 +0530

    Time: 24 ms (77.83%), Space: 38.9 MB (53.18%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> result(n, 1); // Initialize result vector with 1s
-
-        // Calculate prefix products
-        int prefix = 1;
-        for (int i = 0; i < n; ++i) {
-            result[i] = prefix;
-            prefix *= nums[i];
+        int n=nums.size();
+        int arr[n];
+        int arr2[n];
+        arr[0]=nums[0];
+        arr2[n-1]=nums[n-1];
+        for(int i=1;i<n;i++){
+            arr[i]=arr[i-1]*nums[i];
         }
-
-        // Calculate postfix products and multiply with prefix products
-        int postfix = 1;
-        for (int i = n - 1; i >= 0; --i) {
-            result[i] *= postfix;
-            postfix *= nums[i];
+        for(int i=n-2;i>=0;i--){
+            arr2[i]=arr2[i+1]*nums[i];
         }
-
-        return result;
+        nums[0]=arr2[1];
+        nums[n-1]=arr[n-2];
+        for(int i=1;i<n-1;i++){
+            nums[i]=arr[i-1]*arr2[i+1];
+        }
+        return nums;
     }
 };
