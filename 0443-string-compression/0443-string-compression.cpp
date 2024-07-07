ca30fc7cea75db5067f4040b714ca768ba4489d9
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-07-07 11:00:18 +0530

    Time: 4 ms (62.57%), Space: 12.6 MB (24.28%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -4,31 +4,51 @@ public:
         int n = chars.size();
         if (n <= 1) return n;
         
-        int index = 0;
-        int i = 0;
+        // int index = 0;
+        // int i = 0;
         
-        while (i < n) {
-            char currentChar = chars[i];
-            int count = 0;
+        // while (i < n) {
+        //     char currentChar = chars[i];
+        //     int count = 0;
             
-            // Count the number of occurrences of the current character
-            while (i < n && chars[i] == currentChar) {
-                i++;
+        //     // Count the number of occurrences of the current character
+        //     while (i < n && chars[i] == currentChar) {
+        //         i++;
+        //         count++;
+        //     }
+            
+        //     // Write the character to the result
+        //     chars[index++] = currentChar;
+            
+        //     // If count is greater than 1, write the count to the result
+        //     if (count > 1) {
+        //         string countStr = to_string(count);
+        //         for (char c : countStr) {
+        //             chars[index++] = c;
+        //         }
+        //     }
+        // }
+        
+        // return index;
+        int i=0;
+        int index=0;
+        while(i<n){
+            char curr=chars[i];
+            int count=0;
+            while(i<n && curr==chars[i]){
                 count++;
+                i++;
             }
-            
-            // Write the character to the result
-            chars[index++] = currentChar;
-            
-            // If count is greater than 1, write the count to the result
-            if (count > 1) {
-                string countStr = to_string(count);
-                for (char c : countStr) {
-                    chars[index++] = c;
+            chars[index++]=curr;
+            if(count>1){
+                string str=to_string(count);
+                for(int i=0;i<str.length();i++){
+                    chars[index++]=str[i];
                 }
+
             }
+
         }
-        
         return index;
     }
 };
