0c03ff0f87e345706229727e63f76f31ddc28225
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-08-02 12:13:08 +0530

    Time: 15 ms (32.44%), Space: 23.1 MB (22.9%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& fd, int n) {
+        int i = 0;
+        while (i < fd.size() && n > 0) {
+            if (fd[i] == 1) {
+                i += 2;  // Skip the next position if current position has a flower
+            } else {
+                if (i == fd.size() - 1 || fd[i + 1] == 0) {  // Check next position
+                    n--;  // Place a flower
+                    i += 2;  // Skip the next position
+                } else {
+                    i += 3;  // Skip two positions ahead if next position has a flower
+                }
+            }
+        }
+        return n <= 0;
+    }
+};
