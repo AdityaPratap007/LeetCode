79e9688d211936c9dc7eb700a0cc8e6cbba5b9a7
AdityaPratap007 <144808531+AdityaPratap007@users.noreply.github.com>
2024-08-04 11:50:59 +0530

    Time: 76 ms (5.44%), Space: 21.9 MB (37.58%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        std::ios_base::sync_with_stdio(false);
+        std::cin.tie(nullptr);
         int k=0;
         for(int i=0;i<nums.size()-1;i++){
             for(int j=i+1;j<nums.size();j++){
